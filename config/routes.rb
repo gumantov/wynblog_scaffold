aa49c84ab568a9cbd33fa232dc55f609f4806d29
Gustavo Mantovan <gumantovan@gmail.com>
2017-04-06 21:30:57 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :posts do
+    resources :comments
+  end
+  resources :users
+  resources :categories
+
+  root 'posts#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
